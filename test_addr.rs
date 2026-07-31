de6b225ebd56cde4660d782a5f7a78397ca68f1d
ggauranshi-03 <gauranshigupta2000@gmail.com>
2026-07-31 16:05:17 +0530

    deleted test scripts

--- test_addr.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let s = "test";
-    let a: std::result::Result<iroh::EndpointAddr, _> = s.parse();
-}
--- test_parse.rs
+++
@@ -1,5 +0,0 @@
-use std::str::FromStr;
-
-fn main() {
-    println!("Hello");
-}
